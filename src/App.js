39aa8e8b1152e0ba9bef8ad0fb54875721cc876d
Marcos PGE <marcosandrebahia@gmail.com>
2023-03-29 11:44:42 -0400

    Finalizando as configurações de Tema.
    ** Falta configurar o botão para alteração do mesmo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  ColorModeContext,
+  useMode,
+} from "./theme"; /* Passando as configurações de cor para que possam ser acessadas de qualquer lugar*/
+
+import { CssBaseline, ThemeProvider } from "@mui/material"; // passando o CSS importado do Material UI
+
+import Topbar from "./scenes/global/Topbar"
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar></Topbar>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
