af80852c26ab26ddb6fd423904842286318929ff
Marcos PGE <marcosandrebahia@gmail.com>
2023-03-29 17:52:04 -0400

    TopBar
    Incluindo a barra de pesquisa e seu icones

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,4 +1,52 @@
+import { Box, IconButton,  useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import InputBase from "@mui/material/InputBase"
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from "@mui/icons-material/Search";
+
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/*Barra de pesquisa */}
+      <Box
+        display="flex"
+        backgroudColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1,border:1, borderRadius: 2, p:0.5  }} placeholder="Buscar"  />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      {/** Icones */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeIcon />
+          ) : (
+            <LightModeIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <CircleNotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 export default Topbar;
