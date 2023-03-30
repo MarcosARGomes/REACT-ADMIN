c6e9a5d6526cd07b85d190cd53e47a4f901603f3
MarcosARGomes <marcosandrebahia@gmail.com>
2023-03-29 22:42:27 -0300

    Side Bar
    Iniciando a configuração da Side Bar utilizando a biblioteca Pro Sidebar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import {
-  ColorModeContext,
-  useMode,
-} from "./theme"; /* Passando as configurações de cor para que possam ser acessadas de qualquer lugar*/
-
+import { ColorModeContext, useMode } from "./theme";
+/* Passando as configurações de cor para que possam ser acessadas de qualquer lugar*/
 import { CssBaseline, ThemeProvider } from "@mui/material"; // passando o CSS importado do Material UI
+import { Routes,Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar"
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/Team";
+// import Invoices from "./scenes/Invoices";
+// import Contacts from "./scenes/Contacts";
+// import Bar from "./scenes/Bar";
+// import Form from "./scenes/Form";
+// import Line from "./scenes/Line";
+// import Pie from "./scenes/Pie";
+// import FAQ from "./scenes/FAQ";
+// import Geography from "./scenes/Geography";
+// import calendar from "./scenes/calendar";
 
-import Topbar from "./scenes/global/Topbar"
+
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,8 +26,25 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar/>
           <main className="content">
-            <Topbar></Topbar>
+            <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+              {/* <Route path="/team" element={<Dashboard/>}/> */}
+              {/* <Route path="/conacts" element={<Dashboard/>}/> */}
+              {/* <Route path="/invoices" element={<Dashboard/>}/> */}
+              {/* <Route path="/form" element={<Dashboard/>}/> */}
+              {/* <Route path="/bar" element={<Dashboard/>}/> */}
+              {/* <Route path="/pie" element={<Dashboard/>}/> */}
+              {/* <Route path="/line" element={<Dashboard/>}/> */}
+              {/* <Route path="/faq" element={<Dashboard/>}/> */}
+              {/* <Route path="/geography" element={<Dashboard/>}/> */}
+              {/* <Route path="/calendar" element={<Dashboard/>}/> */}
+
+
+
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+// import 'react-pro-sidebar/dist/css/styles.css'
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import PersonIcon from "@mui/icons-material/Person";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import HelpIcon from "@mui/icons-material/Help";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import MenuIcon from "@mui/icons-material/Menu";
+import MapIcon from "@mui/icons-material/Map";
+
 const Sidebar = () => {
-    return <div>SideBar</div>
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false); //Variavel para dizer se o menu está aberto ou fechado
+  const [selected, setSelected] = useState("Dashboard"); //Varialvel que irá indicar a página selecioanada
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`
+        },
+        "& .pro-icon-wrapper":{
+            backgroundColor:"trasparent !important"
+        },
+        "& .pro-inner-item":{
+            padding:"5px 35px 5px 20px !important"
+        },
+        "& .pro-inner-item:hover":{
+            color:"#868dfb !important"
+        },
+        "& .pro-menu-item.active": {
+            color: "#6870fa !important"
+        },
+      }}
+    ></Box>
+  );
+};
 export default Sidebar;
